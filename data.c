c27b824e3c7a8de2f905dfd675c982aa91abc27c
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-18 17:52:10 -0300

    funções de data

--- data.c
+++ data.c
@@ -1,11 +1,21 @@
 #include <stdlib.h>
+#include <string.h>
+
 #include "data.h"
 
 tData lerData() {
-    tData data
-    scanf("%d %d %d", &data.dia, &data.mes, &data.ano)
-    return data
+    tData data;
+    scanf("%d %d %d", &data.dia, &data.mes, &data.ano);
+    return data;
 }
-void igualdadeData() {}
+
+int igualdadeData(tData data1, tData data2) {
+    int bool = 0;
+
+    if (strcmp(data1.dia,data2.dia) && strcmp(data1.mes,data2.mes) && strcmp(data1.ano,data2.ano)){
+        int bool = 1
+    }
+    return bool;
+}
 void menorData() {}
 tData apresentaData() {}
