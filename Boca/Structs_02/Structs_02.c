fd48f80ac180e2c41ad115149fe59f896deae08b
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-21 20:16:08 -0300

    montei a estrutura certa pros arquivos do boca

---
+++ Boca/Structs_02/Structs_02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+   Racional de qualquer numero da forma p/q, sendo p inteiro e q inteiro nao nulo. Vamos
+   convencionar que o campo q de todo racional eh estritamente positivo e que o maximo 
+   divisor comum dos campos p e q eh 1.
+*/
+
+typedef struct {
+   int p, q;
+} racional;
+
+/* INSIRA SEU CODIGO AQUI, CASO PRECISE CRIAR ALGUMA FUNCAO AUXILIAR */
+
+
+
+/* Recebe um racional x e devolve o racional −x */
+racional neg (racional x) {
+	
+	/* INSIRA SEU CODIGO AQUI */
+	
+	
+	
+}
