ba55770bdae38477b1615fcdc718e0120d6dfd79
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-21 20:41:35 -0300

    Adicionei o ex3 do boca

---
+++ Boca/Structs_03/Structs_03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+   Racional de qualquer numero da forma p/q, sendo p inteiro e q inteiro nao nulo. Vamos
+   convencionar que o campo q de todo racional eh estritamente positivo e que o maximo 
+   divisor comum dos campos p e q eh 1.
+*/
+
+typedef struct {
+   int p, q;
+} racional;
+
+/* INSIRA SEU CODIGO AQUI, CASO PRECISE CRIAR ALGUMA FUNCAO AUXILIAR */
+
+
+
+/* Recebe os racionais x e y e devolve o racional que representa a soma de x e y */
+racional soma (racional x, racional y) {
+	
+	/* INSIRA SEU CODIGO AQUI */
+	
+	
+	
+}
