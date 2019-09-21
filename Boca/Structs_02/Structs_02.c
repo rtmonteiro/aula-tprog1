bd16714ca8a4223eb21666bd7d0f31b216432e37
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-21 20:41:19 -0300

    consegui o ex2 do boca

--- Boca/Structs_02/Structs_02.c
+++ Boca/Structs_02/Structs_02.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* 
    Racional de qualquer numero da forma p/q, sendo p inteiro e q inteiro nao nulo. Vamos
@@ -11,14 +13,60 @@ typedef struct {
 } racional;
 
 /* INSIRA SEU CODIGO AQUI, CASO PRECISE CRIAR ALGUMA FUNCAO AUXILIAR */
+racional reduz (int a, int b) {	
+	racional r;
 
+   int mdc = 1;
+   int neg = 0;
+   
+   if ((a<0) != (b<0)) {
+      neg = 1;
+   }
+   r.p = a = abs(a);
+   r.q = b = abs(b);
+
+   int count = a;
+   if (a > b) {
+      count = b;
+   }
+
+   for (int i = count; i >= 1; i--) {
+      if(a%i==0 && b%i==0 && i!=1) {
+         mdc=i*mdc;
+         a=a/i;
+         b=b/i;
+         break;
+      }
+   }
+
+   if (neg==1) {
+      r.p = -(r.p/mdc);
+   } else {
+      r.p = r.p/mdc;
+   }
+   r.q = r.q/mdc;
+
+   return r;
+}
 
 
 /* Recebe um racional x e devolve o racional −x */
 racional neg (racional x) {
-	
-	/* INSIRA SEU CODIGO AQUI */
-	
-	
-	
+   racional r = reduz(x.p, x.q);
+
+   r.p*=-1;
+   
+	return r;
 }
+
+// int main(int argc, char const *argv[])
+// {
+//    int a, b;
+//    scanf("%d", &a);
+//    scanf("%d", &b);
+   
+//    racional r = reduz(a, b);
+//    racional x = neg(r);
+//    printf("%d/%d\n", x.p, x.q);
+//    return 0;
+// }
