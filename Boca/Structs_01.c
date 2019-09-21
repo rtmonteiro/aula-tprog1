cd9782aad181d458121cb9471cd341895b1500ef
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-21 20:08:26 -0300

    acho que acabei o ex1 do boca

--- Boca/Structs_01.c
+++ Boca/Structs_01.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 /* 
@@ -18,35 +19,52 @@ typedef struct {
 /* Recebe inteiros a e b e devolve o racional que representa a/b */
 racional reduz (int a, int b) {	
 	racional r;
-   int mdc=1;
-   int count;
+
+   int mdc = 1;
+   int neg = 0;
+   
+   if ((a<0) != (b<0)) {
+      neg = 1;
+   }
+   r.p = a = abs(a);
+   r.q = b = abs(b);
+
+   int count = a;
    if (a > b) {
       count = b;
-   } else {
-      count = a;
-   }
-   
-   for (int i = 1; i <= count; i++){
-      printf("%d\n", i);
-      if(a%i==0 && b%i==0){
-         mdc=i*mdc;
-      }
-      printf("mdc: %d\n", mdc);
    }
 
-   r.p = a/mdc;
-   r.q = b/mdc;
+   for (int i = count; i >= 1; i--) {
+      // printf("i: %d\n", i);
+      if(a%i==0 && b%i==0 && i!=1) {
+         mdc=i*mdc;
+         a=a/i;
+         b=b/i;
+         // printf("mdc: %d\n", mdc);
+         // printf("a:%d\n", a);
+         // printf("b:%d\n", b);
+         break;
+      }
+   }
+
+   if (neg==1) {
+      r.p = -(r.p/mdc);
+   } else {
+      r.p = r.p/mdc;
+   }
+   r.q = r.q/mdc;
+
    return r;
 }
 
-int main(int argc, char const *argv[])
-{
-   int a, b;
-   scanf("%d", &a);
-   scanf("%d", &b);
+// int main(int argc, char const *argv[])
+// {
+//    int a, b;
+//    scanf("%d", &a);
+//    scanf("%d", &b);
    
-   racional r = reduz(a, b);
-   printf("%d/%d\n", r.p, r.q);
-   return 0;
-}
+//    racional r = reduz(a, b);
+//    printf("%d/%d\n", r.p, r.q);
+//    return 0;
+// }
 
