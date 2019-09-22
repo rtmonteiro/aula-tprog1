3c5e1fbaefc6c758072c72c4f9f0149fc803118c
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-22 16:40:54 -0300

    tentando fazer o mmc dar certo

--- Boca/Structs_03/Structs_03.c
+++ Boca/Structs_03/Structs_03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /* 
    Racional de qualquer numero da forma p/q, sendo p inteiro e q inteiro nao nulo. Vamos
@@ -11,7 +12,64 @@ typedef struct {
 } racional;
 
 /* INSIRA SEU CODIGO AQUI, CASO PRECISE CRIAR ALGUMA FUNCAO AUXILIAR */
+racional reduz (int a, int b) {	
+	racional r;
 
+   int mdc = 1;
+   int neg = 0;
+   
+   if ((a<0) != (b<0)) {
+      neg = 1;
+   }
+   r.p = a = abs(a);
+   r.q = b = abs(b);
+
+   int count = a;
+   if (a > b) {
+      count = b;
+   }
+
+   for (int i = count; i >= 1; i--) {
+      if(a%i==0 && b%i==0 && i!=1) {
+         mdc=i*mdc;
+         a=a/i;
+         b=b/i;
+         break;
+      }
+   }
+
+   if (neg==1) {
+      r.p = -(r.p/mdc);
+   } else {
+      r.p = r.p/mdc;
+   }
+   r.q = r.q/mdc;
+
+   return r;
+}
+
+racional mmc(racional x, racional y) {
+   racional denominadores;
+   int i = y.q;
+   if (x.q>y.q) {
+      i = x.q;
+   }
+   int mmc = 1;
+   printf("%d, %d | %d\n", x.q, y.q, i);
+   do {
+      if (x.q%i==0) {
+         x.q/=i;
+         if (y.q%i==0) {
+            y.q/=i;
+         } 
+         mmc*=i;
+      }
+      i--;
+      printf("%d, %d | %d\n", x.q, y.q, i);
+   } while(x.q != 1 && y.q != 1);
+   printf("mmc: %d", mmc);
+   return denominadores;
+}
 
 
 /* Recebe os racionais x e y e devolve o racional que representa a soma de x e y */
@@ -22,3 +80,27 @@ racional soma (racional x, racional y) {
 	
 	
 }
+
+int main(int argc, char const *argv[])
+{
+   int a, b, c, d;
+   // printf("Sendo p/q\n");
+   scanf("%d", &a);
+   scanf("%d", &b);
+   scanf("%d", &c);
+   scanf("%d", &d);
+   
+   racional x = reduz(a, b);
+   racional y = reduz(c, d);
+   printf("%d/%d\n", x.p, x.q);
+   printf("%d/%d\n", y.p, y.q);
+
+   racional denominadores;
+   denominadores = mmc(x, y);
+
+   return 0;
+}
+// input:
+// 4 1 1 8 -> 129/8
+// 8 2 -2 -16 -> 129/8
+// -8 -2 -2 -16 -> 129/8
