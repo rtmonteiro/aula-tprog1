cc76b252321c3251207be3382cf41c2ba4fe0388
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-22 16:41:19 -0300

    inseri o ex agenda do boca

---
+++ Boca/Agenda/agenda.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct agenda {
+  char* segunda;
+  char* terca;
+  char* quarta;
+  char* quinta;
+  char* sexta;
+};
+
+// Operador ? (age como um if else)
+// int y = x<8 ? 100 : 200;
+
+int main(int argc, char const *argv[]) {
+  return 0;
+}
+11111111 11111111 11111111 11111111 11111111 11111111 10010101
