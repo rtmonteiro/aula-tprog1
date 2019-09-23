be1c2285dae39585827710cb0ab43b0fa036f972
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-23 11:56:37 -0300

    Acho que consegui o ex5 do boca, tenho q enivar depois

--- Boca/Structs_05/Structs_05.c
+++ Boca/Structs_05/Structs_05.c
@@ -11,14 +11,58 @@ typedef struct {
 } racional;
 
 /* INSIRA SEU CODIGO AQUI, CASO DESEJE CRIAR ALGUMA FUNCAO AUXILIAR */
+racional reduz(int a, int b)
+{
+   racional r;
 
+   int mdc = 1;
+   int neg = 0;
+
+   if ((a < 0) != (b < 0))
+   {
+      neg = 1;
+   }
+   r.p = a = abs(a);
+   r.q = b = abs(b);
+
+   int count = a;
+   if (a > b)
+   {
+      count = b;
+   }
+
+   for (int i = count; i >= 1; i--)
+   {
+      if (a % i == 0 && b % i == 0 && i != 1)
+      {
+         mdc = i * mdc;
+         a = a / i;
+         b = b / i;
+         break;
+      }
+   }
+
+   if (neg == 1)
+   {
+      r.p = -(r.p / mdc);
+   }
+   else
+   {
+      r.p = r.p / mdc;
+   }
+   r.q = r.q / mdc;
+
+   return r;
+}
 
 
 /* Recebe os racionais x e y e devolve o racional que representa o quociente de x por y */
 racional quoci (racional x, racional y) {
 
-	/* INSIRA SEU CODIGO AQUI */
+	racional z;
+   z.p = x.p * y.q;
+   z.q = x.q * y.p;
+   z = reduz(z.p, z.q);
 
-	
-		
+   return z;
 }
