a7fe644b8d5510f99c30df3cf2fdcc73b37a8a4d
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-09-23 11:50:03 -0300

    comecei o ex5 do boca

---
+++ Boca/Structs_05/Structs_05.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/* 
+   Racional de qualquer numero da forma p/q, sendo p inteiro e q inteiro nao nulo. Vamos
+   convencionar que o campo q de todo racional eh estritamente positivo e que o maximo 
+   divisor comum dos campos p e q eh 1.   
+*/
+
+typedef struct {
+   int p, q;
+} racional;
+
+/* INSIRA SEU CODIGO AQUI, CASO DESEJE CRIAR ALGUMA FUNCAO AUXILIAR */
+
+
+
+/* Recebe os racionais x e y e devolve o racional que representa o quociente de x por y */
+racional quoci (racional x, racional y) {
+
+	/* INSIRA SEU CODIGO AQUI */
+
+	
+		
+}
