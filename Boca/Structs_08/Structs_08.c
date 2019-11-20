82b36c875729be59a4e514ea7c5c6cccf56b53f6
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2019-11-20 17:08:45 -0300

    Consertei um struct e deu certo

--- Boca/Structs_08/Structs_08.c
+++ Boca/Structs_08/Structs_08.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-struct Vetor {
+typedef struct vetor {
     float x;
     float y;
     float z;
-};
+} Vetor;
 // a × b = [a2b3 − a3b2, a3b1 − a1b3, a1b2 − a2b1]
 
-float dot (struct Vetor v1, struct Vetor v2) {
+float dot (Vetor v1, Vetor v2) {
     float resultado;
     resultado = (v1.x*v2.x) + (v1.y*v2.y) + (v1.z*v2.z);
     return resultado;
 }
 /*
 int main() {
-    struct Vetor v1, v2;
+    Vetor v1, v2;
 
     printf("Vetor 1:");
     scanf("%f", &v1.x);
