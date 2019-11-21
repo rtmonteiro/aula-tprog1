488756f39f854083b93dc358974e0007bc15cdc1
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2019-11-20 21:16:10 -0300

    O boca tá troll com esse struct 09

---
+++ Boca/Structs_09/Structs_09.c
@@ -0,0 +1,46 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "cert-err34-c"
+//
+// Created by ryan on 20/11/2019.
+//
+#include <stdio.h>
+
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} data;
+
+data leData ();
+
+void imprimeData (data atual);
+
+data leData() {
+    data atual = {0, 0, 0};
+//    printf("Digite o dia: ");
+    while(!(atual.dia>=1 && atual.dia <=31)){
+        scanf("%d", &atual.dia);
+    }
+//    printf("\nDigite o mes: ");
+    while(!(atual.mes>=1 && atual.mes <=12)){
+        scanf("%d", &atual.mes);
+    }
+//    printf("\nDigite o ano: ");
+    while(atual.ano < 1){
+        scanf("%d", &atual.ano);
+    }
+    return atual;
+}
+
+void imprimeData(data atual) {
+    printf("A data informada eh %02d/%02d/%d.", atual.dia, atual.mes, atual.ano);
+}
+
+int main (){
+    data atual = leData();
+
+    imprimeData(atual);
+    return 0;
+}
+
+#pragma clang diagnostic pop
